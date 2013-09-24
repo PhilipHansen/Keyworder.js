eaffb6b6b457215edf70cc90c748e133d797e088
Philip Hansen <Philip.Hansen@makerstudios.com>
2013-09-23 20:42:02 -0700

    Changing the storing object of common to be more efficient

--- keyworder.js
+++ keyworder.js
@@ -2,11 +2,16 @@ var keyworder = keyworder || {};
 (function(namespace) {
 
 //generated from http://en.wikipedia.org/wiki/Most_common_words_in_English
-var common = ["the","be","of","to","and","in","a","that","have","I","it","for","not","on","with","he","as","you","do","at","is",
-			"this","but","his","by","from","they","we","say","her","she","or","an","we","will","my","one","all","would","there","their","what",
-			"so","up","out","if","about","who","get","which","go","me","when","make","can","like","time","no","just","him","know","take",
-			"people","into","year","your","good","some","could","them","see","other","than","then","now","look","only","come","its","over","think","also",
-			"back","after","use","two","how","our","work","first","well","way","even","new","want","because","any","these","give","day","most","us"];
+var common = {"the":true,"be":true,"of":true,"to":true,"and":true,"in":true,"a":true,"that":true,"have":true,"I":true,"it":true,
+			"for":true,"not":true,"on":true,"with":true,"he":true,"as":true,"you":true,"do":true,"at":true,"is":true,"this":true,
+			"but":true,"his":true,"by":true,"from":true,"they":true,"we":true,"say":true,"her":true,"she":true,"or":true,"an":true,
+			"we":true,"will":true,"my":true,"one":true,"all":true,"would":true,"there":true,"their":true,"what":true,"so":true,
+			"up":true,"out":true,"if":true,"about":true,"who":true,"get":true,"which":true,"go":true,"me":true,"when":true,"make":true,
+			"can":true,"like":true,"time":true,"no":true,"just":true,"him":true,"know":true,"take":true,"people":true,"into":true,
+			"year":true,"your":true,"good":true,"some":true,"could":true,"them":true,"see":true,"other":true,"than":true,"then":true,
+			"now":true,"look":true,"only":true,"come":true,"its":true,"over":true,"think":true,"also":true,"back":true,"after":true,
+			"use":true,"two":true,"how":true,"our":true,"work":true,"first":true,"well":true,"way":true,"even":true,"new":true,
+			"want":true,"because":true,"any":true,"these":true,"give":true,"day":true,"most":true,"us":true};
 var keywords = [];
 var matchCase = false;
 
@@ -26,26 +31,27 @@ function checkArray() {
 	matchCase: sets wether the keyword matching will be case sensitive or not
 */
 function setOptions(options) {
-	if(options) {
-		checkArray();
-		if(options.common && Array.isArray(options.common)) {
-			if(options.overwrite) { 
-				common = options.common;
-			}
-			else {
-				for(var i = 0; i < options.common.length; i++) {
-					common.push(options.common[i]);
-				}
-			}
-		}
+	if(!options) return;
 
-		if(options.keywords && Array.isArray(options.keywords)) {
-			keywords = options.keywords;
+	checkArray();
+	if(options.common && Array.isArray(options.common)) {
+		if(options.overwrite) { 
+			common = {};
 		}
+		for(var i = 0; i < options.common.length; i++) {
+			var word = options.common[i];
+			if(!common[word]) {
+				common[word] = true;
+			}
+		}
+	}
 
-		if(options.matchCase) {
-			matchCase = options.matchCase;
-		}
+	if(options.keywords && Array.isArray(options.keywords)) {
+		keywords = options.keywords;
+	}
+
+	if(options.matchCase) {
+		matchCase = options.matchCase;
 	}
 }
 namespace.setOptions = setOptions;
@@ -77,7 +83,7 @@ namespace.keywordMatch = keywordMatch;
 
 //takes a string of text, strips the common words from it, and returns a string of the result
 function stripCommonWords(text, stripPunctuation) {
-	if(!text || typeof text != "string" || !common || common.length < 1) {
+	if(!text || typeof text != "string" || !common || Object.keys(common).length < 1) {
 		return text;
 	}
 
@@ -91,7 +97,7 @@ function stripCommonWords(text, stripPunctuation) {
 		if(word[0].match(/"'`/)) {
 			word = word.slice(1);
 		}
-		if(common.indexOf(word) == -1) {
+		if(!common[word]) {
 			returnArray.push((stripPunctuation ? word : arrayText[i]));
 		}
 	}
