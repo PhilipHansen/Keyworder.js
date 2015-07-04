81273bb9518eed7776b8088a0daca1a0e5787d8d
Philip Hansen <philiphansen@philips-mbp.attlocal.net>
2015-07-03 23:35:28 -0700

    Updating to use more modern ES6 conventions

--- keyworder.js
+++ keyworder.js
@@ -15,100 +15,86 @@ var common = {"the":true,"be":true,"of":true,"to":true,"and":true,"in":true,"a":
 var keywords = [];
 var matchCase = false;
 
-//compatibility: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
-function checkArray() {
-	if(!Array.isArray) {
-		Array.isArray = function (vArg) {
-			return Object.prototype.toString.call(vArg) === "[object Array]";
-		};
-	}
-}
-
 /*set the options for the object
-	common: adds more common words to the common array
+	common: adds more common words to the common object
 	overwrite: if this is set, instead of adding new common words, it overwrites them
 	keywords: sets the keywords that we are possible
-	matchCase: sets wether the keyword matching will be case sensitive or not
+	matchCase: sets wether the keyword matching will be case sensitive or not, defaults to false
 */
 function setOptions(options) {
 	if(!options) return;
 
-	checkArray();
 	if(options.common && Array.isArray(options.common)) {
 		if(options.overwrite) { 
 			common = {};
 		}
-		for(var i = 0; i < options.common.length; i++) {
-			var word = options.common[i];
-			if(!common[word]) {
-				common[word] = true;
-			}
-		}
+		options.common.forEach(function(word) {
+			common[word] = true;
+		});
 	}
 
 	if(options.keywords && Array.isArray(options.keywords)) {
 		keywords = options.keywords;
 	}
 
-	if(options.matchCase) {
-		matchCase = options.matchCase;
-	}
+	matchCase = options.matchCase ? true : false;
 }
 namespace.setOptions = setOptions;
 
-//looks through the keywords array for specific keywords that match the input text
+// looks through the keywords array for specific keywords that match the input text
 function keywordMatch(text) {
 	if(!text || typeof text != "string" || !keywords || keywords.length < 1) {
 		return [];
 	}
 
-	var matchingKeywords = [];
 	if(!matchCase) {
 		text = text.toUpperCase();
 	}
 
-	for(var i = 0; i < keywords.length; i++) {
-		var key = keywords[i];
-		if(!matchCase) {
-			key = key.toUpperCase();
-		}
-		if(text.indexOf(key) != -1) {
-			matchingKeywords.push(keywords[i]);
-		}
-	}
+	// return the keywords array filtered of possible keys
+	return keywords.filter(function(word) {
+		if(!matchCase)
+			word = word.toUpperCase();
 
-	return matchingKeywords;
+		return text.indexOf(word) != -1;
+	});
 }
 namespace.keywordMatch = keywordMatch;
 
-//takes a string of text, strips the common words from it, and returns a string of the result
-function stripCommonWords(text, stripPunctuation) {
+// takes a string of text, strips the common words from it, and returns a string of the result
+function stripCommonWords(text, removePunctuation) {
 	if(!text || typeof text != "string" || !common || Object.keys(common).length < 1) {
 		return text;
 	}
 
-	var arrayText = text.split(" ");
-	var returnArray = [];
-	for(var i = 0; i < arrayText.length; i++) {
-		var word = arrayText[i].toLowerCase();
+	// helper function to strip away puncutation
+	function stripAwayPunctuation(word) {
+		word = word.toLowerCase();
 		if(word[word.length-1].match(/[\.,!%\^&\*;:?"'`]/)) {
 			word = word.slice(0, -1);
 		}
 		if(word[0].match(/"'`/)) {
 			word = word.slice(1);
 		}
-		if(!common[word]) {
-			returnArray.push((stripPunctuation ? word : arrayText[i]));
-		}
+		return word;
 	}
 
+	var returnArray = text.split(" ").filter(function(word) {
+		// Only return words that are not in the common library (after being lower cased and had their punctuation removed)
+		return !common[stripAwayPunctuation(word)];
+	});
+
+	// If we want to return items with the punctuation striped away
+	if(removePunctuation) {
+		returnArray = returnArray.map(stripAwayPunctuation);
+	}
 	return returnArray.join(" ");
 }
 namespace.stripCommonWords = stripCommonWords;
 
 //takes a string of text, strips common words out of it and then checks for matching keywords
-function stripAndMatch(text, stripPunctuation) {
-	text = stripCommonWords(text, stripPunctuation);
+function stripAndMatch(text, removePunctuation) {
+	text = stripCommonWords(text, removePunctuation);
 	return keywordMatch(text);
 }
 namespace.stripAndMatch = stripAndMatch;
